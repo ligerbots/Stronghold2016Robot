1bb6c7072b0505784209b4aab5ec439b06cc667d
Charles Frankston <cbf@ligerbots.com>
2016-03-06 17:06:21 -0500

    Define data needed for autonomous.

---
+++ src/FieldInfo.h
@@ -0,0 +1,88 @@
+#pragma once
+// include this file in robot.h once and only once.
+
+/* Ligerbots 2877 Stronghold2016 game
+ *
+ * Information about where various field elements are so we can autonomously navigate
+ * around the field.
+ *
+ * Coordinate system:
+ *  X measures short side of field
+ *  Y measures long side of field
+ *
+ *  In any given competition, the coordinate system is relative to the direction
+ *  our robot starts in. So positive Y always points toward the tower we're attacking,
+ *  and positive X is always +90 from positive Y.
+ *
+ *  ALL MEASUREMENTS ARE IN INCHES!
+ *
+ */
+	static constexpr double RobotLength = 32.0;
+
+	// The back of our robot must start out touching the center starting tape
+	// That tape is 11" from the center of the field
+	static constexpr double StartX = 11.0;
+	// The distance from the starting tape to the starting ramp of each defense
+	// (We've rounded here, it's actually 0.4" longer)
+	static constexpr double StartingTapeToDefense = 86.0;
+	// How far we drive to get from start position to the start of the first defense
+	static constexpr double StartToDefenseDistance = StartingTapeToDefense - (RobotLength + StartX);
+	// After we cross the defense, drive one more foot in autonomous
+	static constexpr double DrivePastDefense = 12.0;
+
+	// Identity array so we can pass pointers to our enum values
+	static int I[32];
+
+	enum StartingPositions {
+		POS_ONE,
+		POS_TWO,
+		POS_THREE,
+		POS_FOUR,
+		POS_FIVE,
+		POS_MAX
+	};
+
+	struct StartingLocations {
+		double x, y;
+	};
+
+	static constexpr StartingLocations startingLocations[] = {
+			{-120.0, StartX },
+			{ -72.0, StartX },
+			{ -24.0, StartX },
+			{  24.0, StartX },
+			{  48.0, StartX }
+	};
+
+	// Target LEFT & RIGHT positions are relative to the direction the robot is facing.
+	enum Targets {
+		TARGET_LEFT,
+		TARGET_CENTER,
+		TARGET_RIGHT,
+		TARGET_MAX
+	};
+
+	struct TargetLocations {
+		double x, y;
+	};
+
+	static constexpr TargetLocations startlocations[] = {
+			{ -75.0, 275.00 },
+			{ 10.0, 225.0 },
+			{ 50.0, 110.0 }
+	};
+
+	enum Defenses {
+			DEF_LOW_BAR,
+			DEF_PORTCULLIS,
+			DEF_CHEVAL,
+			DEF_MOAT,
+			DEF_RAMPARTS,
+			DEF_DRAWBRIDGE,
+			DEF_SALLY_PORT,
+			DEF_ROCK_WALL,
+			DEF_ROUGH_TERRAIN,
+			DEF_MAX
+		};
+
+
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -5,24 +5,7 @@ Robot* Robot::instance = NULL;
 int Robot::ticks;
 bool Robot::isRoadkill = false;
 bool Robot::ROBOT_IS_ABOUT_TO_TIP = false;
-
-int Robot::POS_ONE = 1;
-int Robot::POS_TWO = 2;
-int Robot::POS_THREE = 3;
-int Robot::POS_FOUR = 4;
-int Robot::POS_FIVE = 5;
-int Robot::DEF_LOW_BAR = 6;
-int Robot::DEF_PORTCULLIS = 7;
-int Robot::DEF_CHEVAL = 8;
-int Robot::DEF_MOAT = 9;
-int Robot::DEF_RAMPARTS = 10;
-int Robot::DEF_DRAWBRIDGE = 11;
-int Robot::DEF_SALLY_PORT = 12;
-int Robot::DEF_ROCK_WALL = 13;
-int Robot::DEF_ROUGH_TERRAIN = 14;
-int Robot::TARGET_LEFT = 15;
-int Robot::TARGET_CENTER = 16;
-int Robot::TARGET_RIGHT = 17;
+int Robot::I[];	// our identity array
 
 Robot::Robot() {
 	instance = this;
@@ -47,25 +30,28 @@ void Robot::RobotInit() {
 
 	CommandBase::visionSubsystem->camerasOn();
 
-	mp_position->AddDefault("1", &POS_ONE);
-	mp_position->AddObject("2", &POS_TWO);
-	mp_position->AddObject("3", &POS_THREE);
-	mp_position->AddObject("4", &POS_FOUR);
-	mp_position->AddObject("5", &POS_FIVE);
+	// initialize our identity array
+	for (int i=0; i!=sizeof(I)/sizeof(int); i++) I[i] = i;
 
-	mp_defense->AddDefault("Low Bar", &DEF_LOW_BAR);
-	mp_defense->AddObject("Portcullis", &DEF_PORTCULLIS);
-	mp_defense->AddObject("Cheval de Frise", &DEF_CHEVAL);
-	mp_defense->AddObject("Moat", &DEF_MOAT);
-	mp_defense->AddObject("Ramparts", &DEF_RAMPARTS);
-	mp_defense->AddObject("Drawbridge", &DEF_DRAWBRIDGE);
-	mp_defense->AddObject("Sally Port", &DEF_SALLY_PORT);
-	mp_defense->AddObject("Rock Wall", &DEF_ROCK_WALL);
-	mp_defense->AddObject("Rough Terrain", &DEF_ROUGH_TERRAIN);
+	mp_position->AddDefault("1", &I[POS_ONE]);
+	mp_position->AddObject("2", &I[POS_TWO]);
+	mp_position->AddObject("3", &I[POS_THREE]);
+	mp_position->AddObject("4", &I[POS_FOUR]);
+	mp_position->AddObject("5", &I[POS_FIVE]);
 
-	mp_target->AddDefault("Left", &TARGET_LEFT);
-	mp_target->AddObject("Center", &TARGET_CENTER);
-	mp_target->AddObject("Right", &TARGET_RIGHT);
+	mp_defense->AddDefault("Low Bar", &I[DEF_LOW_BAR]);
+	mp_defense->AddObject("Portcullis", &I[DEF_PORTCULLIS]);
+	mp_defense->AddObject("Cheval de Frise", &I[DEF_CHEVAL]);
+	mp_defense->AddObject("Moat", &I[DEF_MOAT]);
+	mp_defense->AddObject("Ramparts", &I[DEF_RAMPARTS]);
+	mp_defense->AddObject("Drawbridge", &I[DEF_DRAWBRIDGE]);
+	mp_defense->AddObject("Sally Port", &I[DEF_SALLY_PORT]);
+	mp_defense->AddObject("Rock Wall", &I[DEF_ROCK_WALL]);
+	mp_defense->AddObject("Rough Terrain", &I[DEF_ROUGH_TERRAIN]);
+
+	mp_target->AddDefault("Left", &I[TARGET_LEFT]);
+	mp_target->AddObject("Center", &I[TARGET_CENTER]);
+	mp_target->AddObject("Right", &I[TARGET_RIGHT]);
 
 	SmartDashboard::PutData("Auto Position", mp_position);
 	SmartDashboard::PutData("Auto Defense", mp_defense);
