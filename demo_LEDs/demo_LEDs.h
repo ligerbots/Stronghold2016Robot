97bdf7aac97e40e5f7d2fbaa2852e3ad682d630d
erikuhlmann <erikuhlmann@gmail.com>
2016-05-28 13:22:33 -0400

    Add programming demo things

---
+++ demo_LEDs/demo_LEDs.h
@@ -0,0 +1,85 @@
+// this file handles talking to the LEDs and calls animateLEDs
+#include <Adafruit_NeoPixel.h>
+
+// constants
+#define PIN_LEFT 4
+#define PIN_RIGHT 6
+#define LEDS_PER_STRIP 23
+#define LEDS_MODE NEO_GRB+NEO_KHZ800
+
+typedef uint32_t Color;
+
+// variables
+Adafruit_NeoPixel stripLeft(LEDS_PER_STRIP, PIN_LEFT, LEDS_MODE);
+Adafruit_NeoPixel stripRight(LEDS_PER_STRIP, PIN_RIGHT, LEDS_MODE);
+Color red;
+Color orange;
+Color yellow;
+Color green;
+Color blue;
+Color violet;
+Color black;
+Color white;
+int ticks = 0;
+
+void animateLEDs();
+
+void setup() {
+  // LED strip setup
+  stripLeft.begin();
+  stripRight.begin();
+  stripLeft.show();
+  stripRight.show();
+  red = stripLeft.Color(255, 0, 0);
+  orange = stripLeft.Color(255, 165, 0);
+  yellow = stripLeft.Color(255, 255, 0);
+  green = stripLeft.Color(0, 255, 0);
+  blue = stripLeft.Color(0, 0, 255);
+  violet = stripLeft.Color(255, 0, 255);
+  black = stripLeft.Color(0, 0, 0);
+  white = stripLeft.Color(255, 255, 255);
+
+  Serial.begin(9600);
+  Serial.println("Completed setup()");
+}
+
+void loop() {
+  ticks++;
+
+  animateLEDs();
+  
+  stripLeft.show();
+  stripRight.show();
+  delay(20);
+}
+
+Color rgbToColor(int r, int g, int b){
+  return stripLeft.Color(r, g, b);
+}
+
+void setLED(int which, int color){
+  stripLeft.setPixelColor(which, color);
+  stripRight.setPixelColor(which, color);
+}
+
+void animate_fade(Color color){
+  int r = (color >> 16) & 0xFF;
+  int g = (color >> 8) & 0xFF;
+  int b = color & 0xFF;
+  
+  int val_r = (int) (((double) abs((ticks % 50) - 25)) * r / 25.0);
+  int val_g = (int) (((double) abs((ticks % 50) - 25)) * g / 25.0);
+  int val_b = (int) (((double) abs((ticks % 50) - 25)) * b / 25.0);
+  color = stripLeft.Color(val_r, val_g, val_b);
+  for (int i = 0; i < LEDS_PER_STRIP; i++) {
+    stripLeft.setPixelColor(i, color);
+    stripRight.setPixelColor(i, color);
+  }
+}
+
+void animate_all_off() {
+  for (int i = 0; i < LEDS_PER_STRIP; i++) {
+    stripLeft.setPixelColor(i, black);
+    stripRight.setPixelColor(i, black);
+  }
+}
---
+++ demo_LEDs/demo_LEDs.ino
@@ -0,0 +1,22 @@
+#include "demo_LEDs.h"
+
+void animateLEDs(){
+  
+  // moves a line of 10 LEDs across the strips
+  int lineLength = 10;
+  // where is the front of the line of lit LEDs on the strip?
+  int linePosition = ticks % (LEDS_PER_STRIP + lineLength);
+  // what color should the line be?
+  Color lineColor = blue; // or rgbToColor(0, 0, 255)
+  
+  // go through every LED on both strips
+  for (int i = 0; i < LEDS_PER_STRIP; i++) {
+    // if the current LED is in the line, it should be turned on
+    if(i < linePosition && i > linePosition - lineLength){
+      setLED(i, lineColor);
+    } else {
+      setLED(i, black);
+    }
+  }
+}
+
